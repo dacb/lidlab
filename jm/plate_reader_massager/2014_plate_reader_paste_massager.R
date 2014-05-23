d034b22402b20f853d4b87fea3753b31ee793dcb
Janet B. Matsen <jmatsen@d-128-208-236-160.dhcp4.washington.edu>
2014-05-23 14:34:20 -0700

    tidied up  everything and made a demo output file per Beck's suggestion

--- jm/plate_reader_massager/2014_plate_reader_paste_massager.R
+++ jm/plate_reader_massager/2014_plate_reader_paste_massager.R
@@ -1,32 +1,35 @@
-# This function takes a data frame and returns a data frame that is just a CSV that has been loaded
-  # and massages it into a nice format.
-# Save this script into a folder.
-# To operate the function on a csv of your choice, do something like:
-#  source(../../)  (incomplete)
+# This function takes a data frame and returns a data frame that is massaged into a nice format.
+# It is for a csv that results from pasting a the results of SoftMaxPro reading a plate on endpoint mode with any number of shakings before reading it. 
+# See the demo folder for a sample input and output file, as well as instructions for running it.  
+
 plate_reader_paste_massager <- function (raw) {
   d <- raw
   d <- subset(d, select=c("X1","X2","X3","X4","X5","X6","X7","X8","X9","X10","X11","X12"))
   head(d)
   d <- d[rowSums(is.na(d)) != ncol(d),]  # remove rows that are all NA.
-  # When you paste the data into a spreadsheet from SoftMaxPro, the last set of numbers is the calculate number such as average or Vmax.  
+  # When you paste the data into a spreadsheet from SoftMaxPro,
+    # the last set of numbers is the calculate number such as average or Vmax.  
   # Delete this last set, which comes with column labels.
-  nrow(d[1:(nrow(d)-9),])
-  d <-  d[1:(nrow(d)-9),]  # trimmed off the last 9 rows, corresponding to the summary numbers at the bottom. 
+  d <-  d[1:(nrow(d)-9),]  
+       # trimmed off the last 9 rows, corresponding to the summary numbers at the bottom. 
   # rename the columns so they match the 1-12 numbering of 96-well plates. 
   colnames(d) <- c(1:12)
-  d
+  head(d, 20)  # shows the first 20 rows of d in its current state
   # How many sets of data are there?
-  n_measurements <- nrow(d)/8
+  n_measurements <- nrow(d)/8  # divide by 8 because there are 8 rows in a 96-well plate.
   #rep(c("A", "B", "C", "D", "E", "F", "G", "H"), 3)  # see how it works
+  # add a column that labels each row. 
   d$row <- rep(c("A", "B", "C", "D", "E", "F", "G", "H"), n_measurements)
   d
-  # now the rows and columns are labeled correctly.  Next, add a column for the replicate number of the measurement.
+  # now the rows and columns are labeled correctly.  
+  # Next, add a column for the replicate number of the measurement.
   d$rep <- 1:nrow(d) 
   d$rep <- ceiling(d$rep/8)  
   # now I have a replicate number in the column rep
   d_melt <- melt(d, id.vars=c("row", "rep"), variable_name=c("col"))
   d_melt$well <- paste(d_melt$row, d_melt$col, sep="")
-  d_melt <- subset(d_melt, select=-c(row, col))
+  head(d_melt, 15)
+  d_melt <- subset(d_melt, select=-c(row, col))  # removes the columns named "row" and "col"
   #head(d_melt)
   return(d_melt)
 }
--- jm/plate_reader_massager/demo/analysis.R
+++ jm/plate_reader_massager/demo/analysis.R
@@ -1,20 +1,56 @@
 #### Import packages ####
-library(reshape)  # so I can use cast
-library(reshape2)  # cast was depreciated.  Replaced with dcast and acast
-library(ggplot2)
-library(grofit)
-library(RCurl)
+# most of these packages aren't used but are included because they are Janet's favorites. 
+library(reshape)  # for melt and cast
+#library(reshape2)  # cast was depreciated.  Replaced with dcast and acast
+#library(ggplot2)
+library(RCurl) # necessary for getURL below
 library(plyr)
 
-# path of the function I want:
-#   /Dropbox/R_functions/2014_plate_reader_paste_massager.R
-# The path of the CSV I want to massage:
-#   ./raw_data/2014_05_14 Test existing ACS library in 96-well plate - purify with high NaCl - raw_A280.csv
+#getwd()
 
-getwd()
-path.massager <- "../../../../../../../R_functions/2014_plate_reader_paste_massager.R"
+# There are two ways to source an R function in a file.  You can secify an absolute or relative path (see below) *or* get it from github via the command getURL in the package RCurl or by specifying the path to the .R file that contains the function (see below)
+
+# USING THE INTERNET (and getURL from RCurl):  #This is how Janet will use it.  Note, requires internet access!
+  # advantage: uses the most current R file, and can be accessed from any computer.   
+
+#specify the web page for the R file.  NOTE:  want the "raw." part at the beginning!
+source.path <- "https://raw.githubusercontent.com/dacb/lidlab/master/jm/plate_reader_massager/2014_plate_reader_paste_massager.R" 
+
+# use getURL to get the R script
+eval(expr = parse( text = getURL(source.path, ssl.verifypeer=FALSE) )) 
+
+# specify the path to the data you want to use
 path.csv <- "./raw_data/2014_05_14.csv"
+
+# read the csv in
 d <- read.csv(path.csv)
+
+# print the CSV to the screen (for RStudio)
 d
-source(path.massager, local=FALSE, echo=FALSE,chdir=FALSE, print.eval=FALSE)
+#source(path.massager, local=FALSE, echo=FALSE,chdir=FALSE, print.eval=FALSE)
 plate_reader_paste_massager(d)
+d_massaged <- plate_reader_paste_massager(d)
+
+# save to a csv
+write.csv(d_massaged, "./output/demo_output.csv")
+
+# USING PATHS TO ACCESS A COPY OF SoftMax_pro_endpoiint_paste_massager
+
+# To run the function by loading a file from a relative path: 
+# (1) set the path below and uncomment the line
+#path.massager <- "../../../../../../../R_functions/2014_plate_reader_paste_massager.R"
+# (2) 
+# set the path to the CSV you want to run the function on.
+  # In this case it is the path below.  Uncomment the line.  
+# path.csv <- "./raw_data/2014_05_14.csv"i
+# (3) read in the data in the csv  (uncomment line below)
+# d <- read.csv(path.csv)
+# d  # optional: print the raw CSV that hasn't been manipulated before. 
+# (4) use the source command to load the function plate_reader_plate_massager (uncomment again)
+#source(path.massager, local=FALSE, echo=FALSE,chdir=FALSE, print.eval=FALSE)
+# (5) run the function on the data frame.  You could save it by d2 <- plate_reader_paste_massager(d)
+# plate_reader_paste_massager(d)   # or just print it to the screen with this command. 
+
+
+
+
