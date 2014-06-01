2204ca98373fe4f5f7edffe4ff10147900e9a9f5
Janet Matsen <janetmatsen@gmail.com>
2014-06-01 08:54:43 -0700

    updated the demo .R file to use the GitHub URL for the function

--- jm/filling_guide_generator_for_96_well_plates/demo/demo.R
+++ jm/filling_guide_generator_for_96_well_plates/demo/demo.R
@@ -20,19 +20,19 @@ head(d)  # shows top of d
   # advantage: uses the most current R file, and can be accessed from any computer.   
 
 #specify the web page for the R file.  NOTE:  want the "raw." part at the beginning!
-#source.path <- "" 
+source.path <- "https://raw.githubusercontent.com/dacb/lidlab/master/jm/filling_guide_generator_for_96_well_plates/filling_guide_generator_for_96_well_plates.R" 
 
 # use getURL to get the R script
-#eval(expr = parse( text = getURL(source.path, ssl.verifypeer=FALSE) )) 
-
+eval(expr = parse( text = getURL(source.path, ssl.verifypeer=FALSE) )) 
 # to run 
-# cols_for_demo <- c("purification.well","source.flask", "mL.culture", "uL.resin",
-#                    "mM.NaCl.for.lysis.and.wash", "uL.0mM.NaCl.lysis.buffer", 
-#                    "uL.500mM.NaCl.lysis.buffer", "mM.NaCl.in.lysis.and.first.two.washes", 
-#                    "mM.NaCl.in.last.2.washes", "uL.slurry.added", "uL.slurry.to.add")
-# result <- function_96_well_plate_filling_guide(d, cols_for_demo, "purification.well")   # or just print it to the screen with this command. 
-# # write.csv works, but I wanted to omit the row and column names stored in result for saving to a csv, so write.table worked better. 
-# write.table(result, "./output/demo_result.csv", sep=",", row.names=FALSE, col.names=FALSE)
+cols_for_demo <- c("purification.well","source.flask", "mL.culture", "uL.resin",
+                   "mM.NaCl.for.lysis.and.wash", "uL.0mM.NaCl.lysis.buffer", 
+                   "uL.500mM.NaCl.lysis.buffer", "mM.NaCl.in.lysis.and.first.two.washes", 
+                   "mM.NaCl.in.last.2.washes", "uL.slurry.added", "uL.slurry.to.add")
+result <- filling_guide_generator_for_96_well_plates(d, cols_for_demo, "purification.well")   # or just print it to the screen with this command. 
+head(result)
+# write.csv works, but I wanted to omit the row and column names stored in result for saving to a csv, so write.table worked better. 
+write.table(result, "./output/demo_result.csv", sep=",", row.names=FALSE, col.names=FALSE)
 
 # save to a csv
 #write.csv(d_massaged, "./output/demo_output.csv")
