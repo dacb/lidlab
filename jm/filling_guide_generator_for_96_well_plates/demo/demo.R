55e180231cc98ccd8b4fed2fb0d47f88f154980c
Janet Matsen <janetmatsen@gmail.com>
2014-10-04 14:21:55 -0700

    updated demo files

--- jm/filling_guide_generator_for_96_well_plates/demo/demo.R
+++ jm/filling_guide_generator_for_96_well_plates/demo/demo.R
@@ -7,9 +7,10 @@ library(RCurl) # necessary for getURL below
 library(plyr)
 library(stringr)
 
+#### run function ####
 #getwd()
 # specify where the csv is saved
-path.csv <- "./raw_data/2014_05_30.csv"
+path.csv <- "./raw_data/demo_data.csv"
 d <- read.csv(path.csv)
 head(d)  # shows top of d
 
@@ -33,6 +34,15 @@ result <- filling_guide_generator_for_96_well_plates(d, cols_for_demo, "purifica
 head(result)
 # write.csv works, but I wanted to omit the row and column names stored in result for saving to a csv, so write.table worked better. 
 write.table(result, "./output/demo_result.csv", sep=",", row.names=FALSE, col.names=FALSE)
+
+# or if you had a file without plate numbers:
+cols_for_demo <- c("well.position","source.flask", "mL.culture", "uL.resin",
+                   "mM.NaCl.for.lysis.and.wash", "uL.0mM.NaCl.lysis.buffer", 
+                   "uL.500mM.NaCl.lysis.buffer", "mM.NaCl.in.lysis.and.first.two.washes", 
+                   "mM.NaCl.in.last.2.washes", "uL.slurry.added", "uL.slurry.to.add")
+result2 <- filling_guide_generator_for_96_well_plates(d, cols_for_demo, "well.position")   # or just print it to the screen with this command. 
+
+head(result2)
 
 # save to a csv
 #write.csv(d_massaged, "./output/demo_output.csv")
