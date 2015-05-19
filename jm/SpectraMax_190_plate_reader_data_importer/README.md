6d85b6e4be21935f5e92905a9e8e598924bc4743
Janet Matsen <janetmatsen@gmail.com>
2015-05-19 08:05:59 -0700

    initialize new function -- still need to make it work

---
+++ jm/SpectraMax_190_plate_reader_data_importer/README.md
@@ -0,0 +1,30 @@
+This function rearranges column(s) of a data frame into the shape of 96-well blocks, and is intended to convert planning columns into a picture that you can use to load cultures, resin, etc. into 96-well plates.  It accepts well names like H11 (row H, column 11) or 3H11 (plate 3, row H, columns 11)i
+
+This gDoc shows exactly what Janet designed it for, showing input and output
+[140601 filling_guide_generator_for_96_well_plates DEMO](https://docs.google.com/spreadsheets/d/1l60FNFIF2afnxNQVqEfH7sgYI51THDVVXns14lnkdhU/edit#gid=1109366614)
+
+it requires:
+* a csv with some column that has labels spanning A1 to H12 (with the appropriate set of 96 included) or nA1 to nH12 for an nth plate number
+* a list of column names that you want reformatted into your guide
+
+It gives a data.frame you can save to a csv, then do conditional highlighting on in gSpreadsheets or excel.
+
+See the demo package for more detail.  
+
+Required packages:
+library(reshape2)  # for melt, cast
+library(RCurl) # necessary for getURL below
+library(stringr)  #used to get row and column names from well names: str_extract
+
+Arugments:
+
+Argument  | Requirements/Description
+------------- | -------------
+df | The name of the data.frame that contains your columnar plate info.  Each well should be represented by one row, so there should be 96 rows. 
+cols  | A list of the column names, as strings.  Example: c("uL.resin", "mL.culture")
+well.identifier  |  The name of the column that contains the well label of the format A1, B6, C12. 
+
+Warnings:
+* It will break if you give it a column name that doesn't exist
+  * Error 140625: Error in structure(ordered, dim = ns) : 
+  dims [product 96] do not match the length of object [0] 
---
+++ jm/SpectraMax_190_plate_reader_data_importer/demo.R
@@ -0,0 +1,200 @@
+# #### load packages ####
+# #library(reshape)  # so I can use cast
+library(reshape2)  # cast was depreciated.  Replaced with dcast and acast
+# library(ggplot2)
+# #library(grofit)
+# library(RCurl)
+# library(plyr)
+# library(stringr)
+# library(scales)  # 141221 for ggplot2's scale_y_continuous(labels = percent_format())
+# #library(plot3D)
+# # library(relaimpo)
+# # for things like str_split_fixed(df$col, "_split_", 2)
+
+##### PREPARE RAW absorbance & absorbance spectra data DATA #######
+rm(fun_import_raw_spectra)
+fun_import_raw_spectra <- function(filename, path) {
+        # Converts data from SpectraMax190 to columnar format.
+        # Handles end point, kinetic, and spectrum data
+        # See: https://docs.google.com/spreadsheets/d/1XMQXqH1nv7I3iTECpaY-j_JbLKNK9JIwRpfaAqC2y5s/edit?usp=sharing for sample data
+        #
+        # Requires:
+        #   melt (reshape2 package)
+        #       install by: install.packages("reshape2")
+        #       load for each R session: library(reshape2)
+        #
+        # Args:
+        #   filename: the name of a file you want to import
+        #   path: the path to the folder containing the file specified by filename.  e.g. "./raw_data"  (note not "./raw_data/")
+        #
+        # Returns:
+        #   One well scan per row.  One row per time or wavelength as it varies.
+        filename_import <- paste0(path, "/", filename)
+        df.raw <- read.csv(file=filename_import, skip = 0, fill=T, stringsAsFactors = F) #imports the first row as column names
+        # make a data.frame that tells you the row numbers for the start & stop of each chunk
+        End_rows_raw <- grep("~End",df.raw[,1] ) # find all rows that have "~End" as a string in the first column.
+        end_rows <- End_rows_raw -2  # discard the row that is labaled "~End" and the row above that is blank
+        start_rows <- c(1, End_rows_raw[-length(End_rows_raw)]+1) # The first row is a start row.  There are also start rows the row below each row with "~End".
+                # End_rows_raw[-length(End_rows_raw)] removes the last row from the list; there isn't a start row below the last "~End"
+                        # e.g. If there are 5 entries in End_rows_raw, End_rows_raw(-length(End_rows_raw)) returns the first 4 entries.
+        i_info <- data.frame("start_row"=start_rows, "end_row"=end_rows)
+        print(i_info) # lists the row numbers that the chunks start and stop.  Note: the "##BLOCKS= XX" row in the raw CSV does not count.
+        print(paste("nrow of i info:", nrow(i_info)))
+        # break apart the data by chunk
+                #   num.of.reads <- str_extract(colnames(df.raw)[1], pattern = "[0-9]$") # "##BLOCKS= 5" is imported as "X..BLOCKS..5"; this row returns 5.
+        print(paste("1:nrow(i_info): ", 1:nrow(i_info)))
+        for (i in 1:nrow(i_info)){
+                print(paste("i =", i)) # print which set you are on
+                print(i_info[i,])  # prints start_row, end_row
+                i_info_row <- i_info[i,] # the row that has information about the scan.  Wavelength(16) Endpoint/Kinetic (5)
+                d.subset <- df.raw[i_info_row$start_row:i_info_row$end_row,]
+                scan.name <- d.subset[1,2]
+                print(scan.name)
+                data.type <- d.subset[1, 5]
+                print(paste("data type:", data.type))
+                # BREAK APART DEPENDING ON DATA TYPE
+                if (data.type == "Endpoint") {
+                        # This data type has one reading at one wavelength, one temperature, and one time.
+                        plate.temp <- as.numeric(d.subset[3,2])
+                        wavelength <- as.numeric(d.subset[1,16])
+                        # extract the data to melt:
+                        d.temp.col <- data.frame("temperature" = as.numeric(d.subset[3:nrow(d.subset), 2])) # in 2nd column
+                        d.subset <- d.subset[2:nrow(d.subset), 3:ncol(d.subset)] # trim off the non-well/value data
+                        colnames(d.subset) <- d.subset[1,]  # need to define column names
+                        d.subset <- d.subset[-1,]  # delete the row that represents the column names
+                        d.subset <- cbind(d.temp.col, d.subset)
+                        print(head(d.subset, 3))
+                        # add on columns for the values identified
+                        d.subset$scan.name <- scan.name
+                        d.subset$data.type <- data.type
+                        d.subset$time <- NA  # NECESSARY to merge w/ spectrum or kinetic data
+                        melt_columns <- colnames(d.subset)[2:97]  # will pass this list of columns to the melt function
+                        print("melt columns:")
+                        print(melt_columns)
+                        # MELT the data
+                        d.subset <- melt(d.subset, measure.vars = melt_columns, variable.name="scan.well", # scan.well = A1, B1, ...
+                                         value.name = "absorbance")
+                        d.subset$wavelength <- wavelength
+                        d.subset$absorbance = as.numeric(d.subset$absorbance)
+                        d.subset <- d.subset[is.na(d.subset$absorbance)==FALSE, ]
+                        print(head(d.subset))
+                        if(i==1) d = d.subset   # This works; not sure if you can rbind to an empty data frame, so I did this.
+                        if(i>1) d = rbind(d, d.subset)
+                }
+                if (data.type == "Kinetic") {
+                        # This data type has one reading at one wavelength, one temperature, and one time.
+                        # Unfortunately if you have it scan every 20 minutes, the scans will look like this:  20:00:00, 40:00:00, 1:00:00
+                            # this change of format for times > 1 hr is troublesome for parsing because the format changes.
+                        wavelength <- as.numeric(d.subset[1,16])
+                        # extract the data to melt:
+                        d.temp.col <- data.frame("temperature" = as.numeric(d.subset[3:nrow(d.subset), 2])) # in 2nd column
+                        d.time.col <- data.frame("time" = d.subset[3:nrow(d.subset), 1]) # in the 1st column
+                        print("d.time.col:")
+                        print(d.time.col)
+                        print("class of d.time.col:")
+                        print(class(d.time.col))
+                        print("type of d.time.col:")
+                        print(typeof(d.time.col))
+                        d.time.col.parsed <- sapply(strsplit(as.character(d.time.col$time), ":"),  function(x) { x <- as.numeric(x)
+                                                                                                  x[1]+x[2]/60   })
+                        print("d.time.col.parsed:")
+                        print(d.time.col.parsed)
+                        print("d.time.col.parsed[[2]]")
+                        print(d.time.col.parsed[[2]])
+                        print("d.time.col.parsed[[1]]")
+                        print(d.time.col.parsed[[1]])
+                        d.time.step <- d.time.col.parsed[[2]] - d.time.col.parsed[[1]] # The time step is always the same on this machine
+                        print("d time step (minutes):")
+                        print(d.time.step)
+                        # make a column that counts up that number of minutes each row: like c(0:4)*c(10, 10, 10, 10, 10)
+                        d.time.col.minutes <- c(1:length(d.time.col$time)-1)*rep(d.time.step, length(d.time.col$time))
+                        print("d.time.col.minutes:")
+                        print(d.time.col.minutes)
+                        d.subset <- d.subset[2:nrow(d.subset), 3:ncol(d.subset)] # trim off the non-well/value data
+                        colnames(d.subset) <- d.subset[1,]  # need to define column names
+                        d.subset <- d.subset[-1,]  # delete the row that represents the column names
+                        d.subset <- cbind(d.subset, d.temp.col) # bind descriptive columns after the well read values
+                        d.subset <- cbind(d.subset, d.time.col)
+                        # how to bind on a data frame with a new name:
+                                # cbind (data.frame(col1 = c(1,2,3)), d = data.frame(no = c(4,5,6)))
+                        d.subset <- cbind(d.subset, minutes = d.time.col.minutes) # bind descriptive columns after the well read values
+                        print(head(d.subset, 3))
+                        # add on columns for the values identified
+                        melt_columns <- colnames(d.subset)[1:96]  # will pass this list of columns to the melt function
+                        # MELT the data
+                        d.subset <- melt(d.subset, measure.vars = c(melt_columns), variable.name="scan.well", # scan.well = A1, B1, ...
+                                         value.name = "absorbance")
+                        d.subset$wavelength <- wavelength
+                        d.subset$absorbance = as.numeric(d.subset$absorbance)
+                        d.subset <- d.subset[is.na(d.subset$absorbance)==FALSE, ]
+                        d.subset$scan.name <- scan.name
+                        d.subset$data.type <- data.type
+                        print(head(d.subset))
+                        if(i==1) d = d.subset   # This works; not sure if you can rbind to an empty data frame, so I did this.
+                        if(i>1) d = merge(d, d.subset, all=TRUE)
+                }
+                if (data.type == "Spectrum") {
+                        # This data type has one reading per wavelength.  There is an associated temperature value but no time value
+                        # extract the data to melt:
+                        d.temp.col <- data.frame("temperature" = as.numeric(d.subset[3:nrow(d.subset), 2])) # in 2nd column
+                        d.wavelength.col <- data.frame("wavelength" = as.numeric(d.subset[3:nrow(d.subset), 1])) # in the 1st column
+                        d.subset <- d.subset[2:nrow(d.subset), 3:ncol(d.subset)] # trim off the non-well/value data
+                        colnames(d.subset) <- d.subset[1,]  # need to define column names
+                        d.subset <- d.subset[-1,]  # delete the row that represents the column names
+                        d.subset <- cbind(d.subset, d.temp.col) # bind descriptive columns after the well read values
+                        d.subset <- cbind(d.subset, d.wavelength.col) # bind descriptive columns after the well read values
+                        print(head(d.subset, 3))
+                        # add on columns for the values identified
+                        melt_columns <- colnames(d.subset)[1:96]  # will pass this list of columns to the melt function
+                        # MELT the data
+                        d.subset <- melt(d.subset, measure.vars = c(melt_columns), variable.name="scan.well", # scan.well = A1, B1, ...
+                                         value.name = "absorbance")
+                        d.subset$absorbance = as.numeric(d.subset$absorbance)
+                        d.subset <- d.subset[is.na(d.subset$absorbance)==FALSE, ]
+                        d.subset$scan.name <- scan.name
+                        d.subset$data.type <- data.type
+                        print(head(d.subset))
+                        if(i==1) d = d.subset   # This works; not sure if you can rbind to an empty data frame, so I did this.
+                        if(i>1) d = merge(d, d.subset, all=TRUE)
+                }
+        }
+        # convert the time strings to elapsed times
+        # there are some NA values: is.na(tail(d$time))
+        # 150506: this ONLY works for time formats like "30:10" for 30 min 10 sec.  Can't do 1:04:22 for 1 hr, 4 min, 22 sec yet!
+        ## Don't apply this function here; doesn't work for times > 1hr
+        #         d$minutes <- sapply(strsplit(as.character(d$time), ":"),  function(x) { x <- as.numeric(x)
+#                                                                                   x[1]+x[2]/60   })
+        return(d)
+}
+
+filename_CSV <- grep(pattern = "more than 1 hr", x =  list.files("./raw_data"), value = T)
+filename_CSV
+# d <- fun_import_raw_spectra(list.files("./raw_data"), "./raw_data")
+rm(d)
+d <- fun_import_raw_spectra(filename_CSV, "./raw_data")
+
+tmp <- subset(d, scan.well == "A1")
+tmp
+
+time.col <- read.csv("./raw_data/2015_05_15 toy plate reader data to make importing function - time col only.csv",
+                     stringsAsFactors = F)
+head(time.col)
+time.col
+time.col[2,]
+strsplit(as.character(time.col[2,]), ":")
+time.col
+class(time.col)
+typeof(time.col)
+time.col[3,]
+as.character(time.col) # ?? What's up with this? "c(\"0:00\", \"12:00\", \"24:00:00\", \"36:00:00\", \"48:00:00\", \"1:00:00\")"
+strsplit(time.col[2,], ":")
+sapply(strsplit(time.col, ":"),  function(x) { x <- as.numeric(x)
+                                                            x[1]+x[2]/60   })
+#############################
+##########  OLD  ############
+#############################
+
+
+
+
+
